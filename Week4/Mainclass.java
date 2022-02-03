b9fa017acfe6b1a741fc1fffeecdc6e849bcef62
Teemu <92275334+tviitanen@users.noreply.github.com>
2022-02-03 17:14:04 +0200

    final commit

--- Week4/Mainclass.java
+++ Week4/Mainclass.java
@@ -4,7 +4,9 @@ public class Mainclass {
     public static void main(String[] args) {
         String readFile = "input.txt";
         String writeFile = "output.txt";
+        String zipName = "zipinput.zip";
         ReadAndWriteIO RaWIO = new ReadAndWriteIO();
-        RaWIO.readAndWrite(readFile, writeFile);
+        // RaWIO.readAndWrite(readFile, writeFile);
+        RaWIO.readZip(zipName);
     }
 }
--- Week4/ReadAndWriteIO.java
+++ Week4/ReadAndWriteIO.java
@@ -1,33 +1,51 @@
 package week4;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.zip.ZipInputStream;
 
 public class ReadAndWriteIO {
 
-    public void readAndWrite(String readFile, String writeFile){
-        // READ
+    public void readAndWrite(String readFile, String writeFile) {
+
         try {
             try (BufferedReader br = new BufferedReader(new FileReader(readFile))) {
                 String fileLine = "";
-            try (FileWriter fileWriter = new FileWriter(writeFile)) {
-                while ((fileLine = br.readLine()) != null) {
-                    int lineLenght = fileLine.length();
-                    if (lineLenght < 30 && !fileLine.trim().isEmpty() && fileLine.contains("v")) {
-                        fileWriter.write(fileLine+"\n");
+                try (FileWriter fileWriter = new FileWriter(writeFile)) {
+                    while ((fileLine = br.readLine()) != null) {
+                        System.out.println(fileLine);
+
+                        int lineLenght = fileLine.length();
+                        if (lineLenght < 30 && !fileLine.trim().isEmpty() && fileLine.contains("v")) {
+                            fileWriter.write(fileLine + "\n");
+                        }
                     }
-                    }
-                    }
+                    fileWriter.close();
                 }
+                br.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
         }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-        }        
-        catch (IOException e) {
-            System.out.println("An exception occurred while writing the file!");
+    }
+
+    public void readZip(String zipName) {
+        try {
+            ZipInputStream zipStream = new ZipInputStream(new FileInputStream(zipName));
+            zipStream.getNextEntry();
+            byte[] buffer = new byte[2048];
+            int len = zipStream.read(buffer); // Read into the buffer
+            System.out.println(new String(buffer, 0, len));
+            zipStream.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
         }
+
     }
 }
