050fb9beff2b35dcf6f5919629f67f68a3def5d1
Teemu Viitanen <92275334+tviitanen@users.noreply.github.com>
2022-03-15 15:46:26 +0200

    Added singleton method

--- src/Week3/BottleDispenser.java
+++ src/Week3/BottleDispenser.java
@@ -3,13 +3,11 @@ package Week3;
 import java.util.ArrayList;
 
 public class BottleDispenser {
-    private int bottles;
-    private float money;
-
-    ArrayList<Bottle> bottleList;
+    private static float money;
+    private static BottleDispenser instance = null;
+    private static ArrayList<Bottle> bottleList;
     
-    public BottleDispenser() {
-        bottles = 5;
+    private BottleDispenser() {
         money = 0;
         bottleList = new ArrayList<>();
         bottleList.add(new Bottle("Max", "Pepsi", 0.5, 1.8));
@@ -18,11 +16,17 @@ public class BottleDispenser {
         bottleList.add(new Bottle("Zero", "Coca-Cola", 1.5, 2.5));
         bottleList.add(new Bottle("Zero", "Fanta", 0.5, 1.95));
         }
-    public void addMoney() {
+
+    public static BottleDispenser getInstance(){
+        if (instance == null)
+            instance = new BottleDispenser(); 
+        return instance;
+    }
+    protected void addMoney() {
         money += 1;
         System.out.println("Klink! Added more money!");
     }
-    public void buyBottle(int purchase) {
+    protected void buyBottle(int purchase) {
         Bottle b = bottleList.get(purchase-1);
         if (money >= b.getPrice()) {
             money -= b.getPrice();
@@ -35,13 +39,13 @@ public class BottleDispenser {
         }
     }
 
-    public void returnMoney() {
+    protected void returnMoney() {
         String formatted_money = String.format("%.2f", money);
         System.out.println("Klink klink. Money came out! You got "+ formatted_money + "€ back");
         money = 0;
     }
 
-    public void printBottles(){
+    protected void printBottles(){
     for (int i = 0; i < bottleList.size(); i++) {
         System.out.println(i+1 + ". Name: " + bottleList.get(i).getManufacturer() + " " + bottleList.get(i).getName());
         System.out.println("\tSize: " + bottleList.get(i).getSize() + "\tPrice: " + bottleList.get(i).getPrice());
